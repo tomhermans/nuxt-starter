75b327c8716ecbd23e31781362dc081ddec1201b
Tom Hermans <tomhermans@gmail.com>
2018-08-13 23:02:00 +0200

    adding link to WP APi (https://www.superawesomegood.com/2017/10/23/building-a-blog-with-wp-api-nuxt/)

---
+++ components/posts/PostList.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="post-list">
+    <li v-for="post in posts" :key="post.id" class="post-list-item">
+      <PostExcerpt :post="post" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import PostExcerpt from './PostExcerpt.vue'
+
+export default {
+  props: {
+    posts: { type: Array }
+  },
+  components: {
+    PostExcerpt
+  }
+}
+</script>
+
+<style scoped>
+.post-list {
+  padding: 0;
+}
+
+.post-list-item {
+  list-style: none;
+  margin: 10px 0;
+  border: 1px solid black;
+  padding: 0 1rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,12 @@
       <h2>No idea where this is going next</h2>
       <p>Let's add some lorem ipsum</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempor ex felis, a laoreet elit ullamcorper eget. Ut quis euismod urna, quis eleifend nisi. Praesent libero justo, bibendum blandit bibendum vel, imperdiet sed dui. Ut augue est, placerat sed purus in, viverra auctor enim. Aenean eleifend tortor ac est pulvinar aliquam. Maecenas lacinia eros sed egestas tristique. Vivamus pharetra ex maximus velit pellentesque pharetra. Nulla augue magna, malesuada vitae dolor nec, maximus varius diam. Sed faucibus dui ipsum, non fermentum dolor hendrerit tincidunt. Praesent eu dolor et dolor volutpat aliquam. Nulla suscipit faucibus purus at sollicitudin. Proin elementum, augue sit amet fermentum convallis, elit justo pretium magna.</p>
+
+      <hr>
+      <div class="posts-container">
+          <PostList :posts="posts" />
+      </div>
+      <hr>
     </main>
     <aside class="grid-aside">
       aside
@@ -12,3 +18,29 @@
 
   </div>
 </template>
+
+<script>
+  import PostList from '~/components/posts/PostList.vue'
+  import wp from '~/lib/wp'
+
+  export default {
+    async asyncData ({ params }) {
+      return wp.posts()
+    },
+    components: {
+      PostList
+    }
+  }
+</script>
+
+<style>
+  .posts-container {
+    margin: 1rem auto;
+  }
+</style>
+
+
+
+
+
+
