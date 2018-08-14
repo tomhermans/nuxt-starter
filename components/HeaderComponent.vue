03baf12def448f55b7bfb8572d37be20240cc08c
Tom Hermans <tomhermans@gmail.com>
2018-08-14 07:45:08 +0200

    header component

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,8 +1,21 @@
 <template>
   <header class="grid-header">
     <img class="header__logo" src="../assets/img/logo.svg" alt="">
-    header goes here.
+    <h1><nuxt-link to="/">title site todo replace</nuxt-link></h1>
+    <p>site desc todo replace</p>
   </header>
 </template>
 
+<script>
+export default {
+  props: {
+    siteData: { type: Object }
+  }
+}
+</script>
 
+<style scoped>
+  h1 {
+    display: inline;
+  }
+</style>
